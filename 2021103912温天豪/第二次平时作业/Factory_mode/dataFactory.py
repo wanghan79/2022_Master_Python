bdbc42a69bb731f6f38974e835e6b8289f98b09c
wth0425 <91104456+wth0425@users.noreply.github.com>
2022-05-31 15:53:08 +0800

    Delete 2021103912温天豪 directory

--- 2021103912温天豪/第二次平时作业/Factory_mode/dataFactory.py
+++
@@ -1,7 +0,0 @@
-class dataFactory(object):
-
-    def __init__(self):
-        self.__name = 'dataFactory'
-
-    def sampling(self, **kwargs):
-        raise Exception("This is a base factory")
--- 2021103912温天豪/第二次平时作业/Factory_mode/main.py
+++
@@ -1,9 +0,0 @@
-from dataFactoryInterface import dataFactoryInterface
-
-if __name__ == '__main__':
-    interface = dataFactoryInterface()
-
-    obj = interface.create('int')
-    paras = {'datarange': (0, 10), 'num': 5}
-    result = obj.sampling(**paras)
-    print(result)
