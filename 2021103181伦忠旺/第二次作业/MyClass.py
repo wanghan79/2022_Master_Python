5a7c659da81f62a7d423f3818de40eb77efc4184
Lunavan <1787648106@qq.com>
2022-06-05 20:16:41 +0800

    Add files via upload

---
+++ 2021103181伦忠旺/第二次作业/MyClass.py
@@ -0,0 +1,8 @@
+
+class MyClass(object):
+    classVar=456
+
+    def __init__(self,index):
+        self.menVar = index
+        self.menlist = list()
+
---
+++ 2021103181伦忠旺/第二次作业/strSampling.py
@@ -0,0 +1,26 @@
+import random
+
+from dataFactory import dataFactory
+
+
+class strSampling(dataFactory):
+    def __init__(self):
+        self.__name = "strSampling"
+
+
+
+    def sampling(self,**kwargs):
+        result = list()
+
+        if not isinstance(kwargs.get('num'),int):  # 判断是否为整型数字
+            raise Exception("num必须为整型数字")
+        if not isinstance(kwargs.get('datarange'),str):
+            raise Exception("随机数范围必须为字符串")
+        if not isinstance(kwargs.get('strlen'),int):  # 判断是否为整型数字
+            raise Exception("长度必须为整型数字")
+
+        for _ in range(0,kwargs.get('num')):
+            it = iter(kwargs.get('datarange'))
+            tmp = ''.join(random.SystemRandom().choice(kwargs.get('datarange')) for _ in range(kwargs.get("strlen")))
+            result.append(tmp)
+        return result
