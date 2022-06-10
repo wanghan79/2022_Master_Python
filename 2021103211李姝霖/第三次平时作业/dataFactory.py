90cabd8a9aa579df8660bb87de7837146c876f8d
Lily_su <58245234+Lily-su@users.noreply.github.com>
2022-06-10 23:20:34 +0800

    Add files via upload

---
+++ 2021103211李姝霖/第三次平时作业/dataFactory.py
@@ -0,0 +1,6 @@
+class dataFactory(object):
+    def __int__(self):
+        self.__name = "dataFactory"
+
+    def sampling(self, **kwargs):
+        print("This is a base factory, no implement to sample data!")
