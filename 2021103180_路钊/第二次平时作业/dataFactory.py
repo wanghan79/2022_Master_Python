ceaf9104c60f151c05249983fb6513a5f171a9cd
954684226 <40624259+954684226@users.noreply.github.com>
2022-06-12 12:51:48 +0800

    Add files via upload

---
+++ 2021103180_路钊/第二次平时作业/dataFactory.py
@@ -0,0 +1,6 @@
+class dataFactory(object):
+    def __init__(self):
+        self.__name = "dataFactory"
+
+    def sampling(self, **kwargs):
+        raise "This is a base factory,no implement to sample data!"
