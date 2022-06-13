c9786bec0d099481780e8b54616c752ce2eaa2a4
2021103209孔德露 <46191115+VianaKong@users.noreply.github.com>
2022-06-13 22:42:07 +0800

    Delete 2021103209孔德露/2021103209孔德露 directory

--- 2021103209孔德露/2021103209孔德露/第三次平时作业/ACC&MCC/dataFactory.py
+++
@@ -1,6 +0,0 @@
-class dataFactory(object):
-    def __int__(self):
-        self.__name = "dataFactory"
-
-    def sampling(self, **kwargs):
-        print("This is a base factory, no implement to sample data!")
