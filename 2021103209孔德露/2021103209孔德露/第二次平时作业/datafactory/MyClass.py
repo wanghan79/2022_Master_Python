aec17edadfaaff4af386c190293aeca02655a98a
2021103209孔德露 <46191115+VianaKong@users.noreply.github.com>
2022-06-13 22:41:33 +0800

    Add files via upload

---
+++ 2021103209孔德露/2021103209孔德露/第二次平时作业/datafactory/MyClass.py
@@ -0,0 +1,8 @@
+class MyClass(object):
+
+    classvar = 456
+
+    def __init__(self,index):
+        self.memVar = index
+        self.memlist = list()
+
---
+++ 2021103209孔德露/2021103209孔德露/第二次平时作业/datafactory/strSampling.py
@@ -0,0 +1,17 @@
+import random
+
+
+class strSampling(object):
+    """
+    字符串随机数生成
+    """
+
+    def __init__(self):
+        self._name = "strSampling"
+
+    def sampling(self, **kwargs):
+        result = list()
+        for _ in range(0, kwargs.get('num')):
+            tmp = ''.join(random.SystemRandom().choice(kwargs.get('datarange')) for _ in range(kwargs.get("strlen")))
+            result.append(tmp)
+        return result
