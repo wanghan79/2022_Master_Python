d1debc3cd3bc3774ac8e0f92ccd433ae5e300d41
2021103179刘凯 <86090978+Karity@users.noreply.github.com>
2022-06-13 11:13:15 +0800

    Add files via upload

---
+++ 2021103179刘凯/第二次作业/strSampling.py
@@ -0,0 +1,15 @@
+import random
+
+from dataFactory import dataFactory
+
+
+class strSampling(dataFactory):
+    def __init__(self):
+        self.__name = "strSampling"
+
+    def sampling(self, **kwargs):
+        result = list()
+        for _ in range(0, kwargs.get('num')):
+            tmp = ''.join(random.SystemRandom().choice(kwargs.get('datarange')) for _ in range(kwargs.get('strlen')))
+            result.append(tmp)
+        return result
---
+++ 2021103179刘凯/结课大作业/callDealer.py
@@ -0,0 +1,27 @@
+from largeXMLDealer import largeXMLDealer
+
+
+def dealwithElement(elem):
+    dict_elem = {}
+    child_list = []
+    if isinstance(elem, object):
+        dict_elem['tag'] = elem
+        for child in elem:
+            if isinstance(child, object):
+                child_list.append(dealwithElement(child))
+        if child_list == []:
+            dict_elem['child'] = None
+        else:
+            dict_elem['child'] = child_list
+    return dict_elem
+
+@largeXMLDealer
+def DealerStrat(*args, **kwargs):
+    print('start')
+
+
+if __name__ == "__main__":
+    fileName = './P00734.xml'
+    # elemTag = 'protein'
+    elemTag = 'component'
+    DealerStrat(fileName, elemTag, dealwithElement)
