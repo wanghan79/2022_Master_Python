fa87937e3c66a25817afac48a120668775ed7931
wangyx19 <100987084+wangyx19@users.noreply.github.com>
2022-06-16 22:19:26 +0800

    Add files via upload

---
+++ 2021103190王艺璇/第二次平时作业/datafactory.py
@@ -0,0 +1,7 @@
+class dataFatory(object):
+
+    def __init__(self):
+        self.__name = "dataFatory"
+
+    def sampling(self, **kwargs):
+        raise "This is a base fatory"
---
+++ 2021103190王艺璇/第二次平时作业/strsampling.py
@@ -0,0 +1,23 @@
+import random
+
+import datafactory
+
+
+class strSampling(datafactory.dataFatory):
+
+
+    def __init__(self):
+        self.__name = "strSampling"
+
+    def sampling(self, **kwargs):
+        result = list()
+        if ('strlen' or 'datarange') not in kwargs:
+            result.append("err")
+            result.append("parameter not right,please input again")
+            return result
+        for _ in range(0, kwargs.get('num')):
+            tmp = ''.join(random.SystemRandom().choice(kwargs.get('datarange')) for _ in range(kwargs.get('strlen')))
+            result.append(tmp)
+        return result
+
+
