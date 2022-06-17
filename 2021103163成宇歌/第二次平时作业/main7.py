f856ec25e338f09d2e7f836013bad46bdd7a6528
2021103163成宇歌 <48313874+nenucyg@users.noreply.github.com>
2022-06-18 01:54:15 +0800

    Delete 2021103163成宇歌/第二次平时作业 directory

--- 2021103163成宇歌/第二次平时作业/main7.py
+++
@@ -1,8 +0,0 @@
-import m1
-if __name__=="__main__":
-    interface=m1.dataFactoryInterface()
-    obj = interface.create("int")
-    paras={"datarange":(0,10),"num":5}
-    result=obj.sampling(**paras)
-    for item in result:
-        print(item)
