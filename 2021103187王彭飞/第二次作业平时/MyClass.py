6c9055deab5c7adf179ee22220a02d9b19063b48
PENGFly2022 <101027837+PENGFly2022@users.noreply.github.com>
2022-06-17 15:57:44 +0800

    Delete 2021103187王彭飞/第二次作业平时 directory

--- 2021103187王彭飞/第二次作业平时/MyClass.py
+++
@@ -1,7 +0,0 @@
-class MyClass(object):
-    def __init__(self, index):
-        self.__name = "MyClass"
-        self.memva = index
-    # def __str__(self):
-    #     return "this is object str " + str(self.memvar)
-
--- 2021103187王彭飞/第二次作业平时/dataFactory.py
+++
@@ -1,7 +0,0 @@
-class dataFactory(object):
-
-    def __init__(self) -> None:
-        self.__name = "dataFactory"
-
-    def sampling(self, **kwargs):
-        raise "this is a  base factory, no implement to sample data!"
