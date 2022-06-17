a8833a988cab901f3ce03a5f7a4b3a7e1ceca744
2021103163成宇歌 <48313874+nenucyg@users.noreply.github.com>
2022-06-18 01:54:46 +0800

    Add files via upload

---
+++ 2021103163成宇歌/第二次平时作业/strSampling.py
@@ -0,0 +1,11 @@
+import random
+from m2 import dataFactory
+class strSampling(dataFactory):
+    def __init__(self):
+        self.__name="strSampling"
+    def sampling(self,**kwargs):
+        result = list()
+        for _ in range(0,kwargs.get('num')):
+            tmp= ''.join(random.SystemRandom().choice(kwargs.get('datarange')) for _ in range(kwargs.get('strlen')))
+            result.append(tmp)
+        return result
