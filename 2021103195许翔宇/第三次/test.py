46a27c3c69eff7d12e1b01891806cfb75f09e7e5
erduo111 <100890739+erduo111@users.noreply.github.com>
2022-06-18 15:11:54 +0800

    Create test.py

---
+++ 2021103195许翔宇/第三次/test.py
@@ -0,0 +1,23 @@
+def decorator(func):
+    def inner(*args, **kwargs):
+        print('before...........')
+        res = func(*args, **kwargs)
+        print('after............')
+        return res
+    return inner
+
+class test(object):
+    def __init__(self):
+        self.__name = "test"
+
+    def run(self):
+        print('run...............')
+        return 0
+
+if __name__ == "__main__":
+    t = test()
+    t.run()
+    run = decorator(t.run)
+    run ()
+    # run.__name__
+    # 此时decorator叫做装饰器
